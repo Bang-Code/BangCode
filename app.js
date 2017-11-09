0ff896d92a46a56e5f392d4d660ed1f81dd2762b
rjtj2007 <teacherman1@gmail.com>
2017-11-08 20:08:51 -0800

    added to README.md

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.getElementById("dropbtn").addEventListener("mouseout", mouseOut);
 
 // show/hide the dropdown on mouse over/out
 document.getElementById("myDropdown").addEventListener("mouseout", mouseOut);
+
 function mouseOut() {
-    console.log('hello');
-    document.getElementById("myDropdown").classList.toggle("show");    
+  document.getElementById("myDropdown").classList.toggle("show");
 }
