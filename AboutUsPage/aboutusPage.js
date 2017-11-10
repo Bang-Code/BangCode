c4dbaa6f32b2327339219aae0a1e3f749e93e41d
rjtj2007 <teacherman1@gmail.com>
2017-11-09 20:04:00 -0800

    all content pages added and linked to landing page

--- AboutUsPage/aboutusPage.js
+++ AboutUsPage/aboutusPage.js
@@ -28,3 +28,42 @@ new Link('Zen Garden','http://www.csszengarden.com/','Crunchy');
 new Link('Color','https://www.w3schools.com/css/css_colors.asp','colour');
 new Link('box model','https://css3gen.com/css-box-model/','boxes');
 console.log(allLinks);
+
+
+
+
+
+
+// Navigation bar
+document.getElementById("dropbtn").addEventListener("mouseover", mouseOut);
+document.getElementById("dropbtn").addEventListener("mouseout", mouseOut);
+
+// show/hide the dropdown on mouse over/out
+document.getElementById("myDropdown").addEventListener("mouseout", mouseOut);
+
+function mouseOut() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Back To Top functionality
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  if (window.scrollY != 0) {
+    setTimeout(function() {
+      window.scrollTo(0, window.scrollY - 900);
+      topFunction();
+    }, 40);
+  }
+}
